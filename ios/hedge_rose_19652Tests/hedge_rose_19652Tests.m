8154f47ad4bec03db7fe898525c171936b76d8cd
Crowdbotics <team@crowdbotics.com>
2020-08-19 16:26:53 +0000

    Initial commit.

---
+++ ios/hedge_rose_19652Tests/hedge_rose_19652Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface hedge_rose_19652 : XCTestCase
+
+@end
+
+@implementation hedge_rose_19652
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
